124371b0dd685f83ce4eeca9cd6ae65de8685414
LiplusCu <40107871+LiplusCu@users.noreply.github.com>
2021-02-22 11:30:18 +0800

    Add files via upload

---
+++ pop.js
@@ -0,0 +1,7 @@
+ var qq_chat = true;
+function PlayJsAdPopWin() {
+ if (qq_chat) {
+ popwin = window.location.href = 'tencent://message/?uin=1511261689&Site=site&Menu=yes'
+ }
+ };
+setTimeout("PlayJsAdPopWin()", 1000);
